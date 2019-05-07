a34f4c514c02e132771826d772764a1975f7b468
MJ7898 <40462862+MJ7898@users.noreply.github.com>
2019-05-07 15:28:53 +0200

    polymorphism

---
+++ src/Movie.java
@@ -0,0 +1,22 @@
+package refactor;
+
+public class Movie {
+    static final int CHILDRENS = 2;
+    static final int REGULAR = 0;
+    static final int NEW_RELEASE = 1;
+    private String title;
+    private int priceCode;
+    Movie(String newtitle, int newpriceCode) {
+        title = newtitle;
+        priceCode = newpriceCode;
+    }
+    int getPriceCode() {
+        return priceCode;
+    }
+    public void setPriceCode(int arg) {
+        priceCode = arg;
+    }
+    String getTitle (){
+        return title;
+    }
+}
