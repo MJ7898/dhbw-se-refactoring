eac02f2cc08c9dd98feb64541b6ddb80e3b32eb1
MJ7898 <40462862+MJ7898@users.noreply.github.com>
2019-05-07 15:30:23 +0200

    changed values to private

---
+++ src/Rental.java
@@ -0,0 +1,16 @@
+package refactor;
+
+class Rental {
+    private Movie movie;
+    private int daysRented;
+    Rental(Movie newmovie, int newdaysRented) {
+        movie = newmovie;
+        daysRented = newdaysRented;
+    }
+    int getDaysRented() {
+        return daysRented;
+    }
+    Movie getMovie() {
+        return movie;
+    }
+}
